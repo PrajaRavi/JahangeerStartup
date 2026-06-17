811e58601a79315babec2b951ee5743598e867be
Amit Prajapati <you@example.com>
2026-06-17 22:53:26 +0530

    update

--- whatsapp-clone/src/components/Services.tsx
+++ whatsapp-clone/src/components/Services.tsx
@@ -10,57 +10,56 @@ import LaundryServiceModalDemo, { type CurrServiceType } from "./ServiceDetails"
 const services = [
   {label:"Wash & Fold",id:"serv1",icon:Icon1,Items:[
   { id: 1, name: "Shirt", price: 15 },
-  { id: 2, name: "T-Shirt", price: 12 },
-  { id: 3, name: "Trouser", price: 20 },
-  { id: 4, name: "Jeans", price: 25 },
-  { id: 5, name: "Saree", price: 60 },
-  { id: 6, name: "Blazer", price: 50 },
-  { id: 7, name: "Kurta", price: 30 },
+  { id: 2, name: "T-Shirt", price: 10 },
+  { id: 3, name: "Trouser", price: 15 },
+  { id: 4, name: "Jeans", price: 10 },
+  { id: 5, name: "Saree(Regular)", price: 30 },
+  { id: 6, name: "Saree(Etc)", price: 120 },
+  { id: 7, name: "Dress material", price: 20 },
 ]},
-  {label:"Dry Cleaning",id:"serv2",icon:Icon2,Items:[
-  { id: 1, name: "Shirt", price: 15 },
-  { id: 2, name: "T-Shirt", price: 12 },
-  { id: 3, name: "Trouser", price: 20 },
-  { id: 4, name: "Jeans", price: 25 },
-  { id: 5, name: "Saree", price: 60 },
-  { id: 6, name: "Blazer", price: 50 },
-  { id: 7, name: "Kurta", price: 30 },
-]},
-  {label:"Wash & Iron",id:"serv3",icon:Icon3,Items:[
-  { id: 1, name: "Shirt", price: 15 },
-  { id: 2, name: "T-Shirt", price: 12 },
-  { id: 3, name: "Trouser", price: 20 },
-  { id: 4, name: "Jeans", price: 25 },
-  { id: 5, name: "Saree", price: 60 },
-  { id: 6, name: "Blazer", price: 50 },
-  { id: 7, name: "Kurta", price: 30 },
-]},
+//   {label:"Dry Cleaning",id:"serv2",icon:Icon2,Items:[
+//   { id: 1, name: "Shirt", price: 15 },
+//   { id: 2, name: "T-Shirt", price: 12 },
+//   { id: 3, name: "Trouser", price: 20 },
+//   { id: 4, name: "Jeans", price: 25 },
+//   { id: 5, name: "Saree", price: 60 },
+//   { id: 6, name: "Blazer", price: 50 },
+//   { id: 7, name: "Kurta", price: 30 },
+// ]},
+
+//   {label:"Wash & Iron",id:"serv3",icon:Icon3,Items:[
+//   { id: 1, name: "Shirt", price: 15 },
+//   { id: 2, name: "T-Shirt", price: 12 },
+//   { id: 3, name: "Trouser", price: 20 },
+//   { id: 4, name: "Jeans", price: 25 },
+//   { id: 5, name: "Saree", price: 60 },
+//   { id: 6, name: "Blazer", price: 50 },
+//   { id: 7, name: "Kurta", price: 30 },
+// ]},
+
   {label:"Heavy Wash service",id:"serv4",icon:Icon4,Items:[
-  { id: 1, name: "Shirt", price: 15 },
-  { id: 2, name: "T-Shirt", price: 12 },
-  { id: 3, name: "Trouser", price: 20 },
-  { id: 4, name: "Jeans", price: 25 },
-  { id: 5, name: "Saree", price: 60 },
-  { id: 6, name: "Blazer", price: 50 },
-  { id: 7, name: "Kurta", price: 30 },
+  { id: 8, name: "Curtain", price: 25 },
+  { id: 9, name: "Bad sheet", price: 20 },
+  { id: 10, name: "Pillow covers", price: 12 },
+  ]},
+
+  {label:"Bag cleaning",id:"serv5",icon:Icon3,Items:[
+  { id: 20, name: "Sch. Bag/Office Bag", price: 80 },
+  { id: 21, name: "Travel Bag", price: 100 },
+  // { id: 22, name: "Trouser", price: 20 },
+  // { id: 4, name: "Jeans", price: 25 },
+  // { id: 5, name: "Saree", price: 60 },
+  // { id: 6, name: "Blazer", price: 50 },
+  // { id: 7, name: "Kurta", price: 30 },
 ]},
-  {label:"Bag cleaning",id:"serv5",icon:Icon5,Items:[
-  { id: 1, name: "Shirt", price: 15 },
-  { id: 2, name: "T-Shirt", price: 12 },
-  { id: 3, name: "Trouser", price: 20 },
-  { id: 4, name: "Jeans", price: 25 },
-  { id: 5, name: "Saree", price: 60 },
-  { id: 6, name: "Blazer", price: 50 },
-  { id: 7, name: "Kurta", price: 30 },
-]},
-  {label:"Shoe Cleaning",id:"serv6",icon:"../src/assets/sport-shoe.png",Items:[
-  { id: 1, name: "Shirt", price: 15 },
-  { id: 2, name: "T-Shirt", price: 12 },
-  { id: 3, name: "Trouser", price: 20 },
-  { id: 4, name: "Jeans", price: 25 },
-  { id: 5, name: "Saree", price: 60 },
-  { id: 6, name: "Blazer", price: 50 },
-  { id: 7, name: "Kurta", price: 30 },
+  {label:"Shoe Cleaning",id:"serv6",icon:Icon5,Items:[
+  { id: 1, name: "Sports shoes", price: 80 },
+  { id: 2, name: "Sneakers", price: 80 },
+  { id: 3, name: "Casual shoes", price: 60 },
+  // { id: 4, name: "Jeans", price: 25 },
+  // { id: 5, name: "Saree", price: 60 },
+  // { id: 6, name: "Blazer", price: 50 },
+  // { id: 7, name: "Kurta", price: 30 },
 ]},
 ];
 
@@ -71,7 +70,7 @@ export default function Services() {
   
   return (
     <>
-    <div className="text-black fixed top-0 left-0">
+    <div className="text-black fixed top-0 z-50 left-0">
 
           {OpenService&&<LaundryServiceModalDemo CurrService={SelectedService as CurrServiceType} open={OpenService} setOpen={setOpenService}/>}
     </div>
